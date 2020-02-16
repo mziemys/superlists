35e37e3e44795232faa4f554b6e100ba4bf2cc6c
Martynas <mziemys@gmail.com>
2020-02-16 17:17:09 +0200

    Custom passwordless auth backend + custom user model

---
+++ accounts/views.py
@@ -0,0 +1,33 @@
+import sys
+from django.shortcuts import redirect
+from accounts.models import Token
+from django.core.mail import send_mail
+from django.contrib import messages, auth
+from django.core.urlresolvers import reverse
+
+# Create your views here.
+def send_login_email(request):
+    email = request.POST['email']
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse('login') + '?token=' + str(token.uid)
+    )
+    message_body = f'Use this link to log in:\n\n{url}'
+    send_mail(
+        'Your login link for Superlists',
+        message_body,
+        'noreply@inbox.lt',
+        [email],
+    )
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in."
+    )
+    return redirect('/')
+
+
+def login(request):
+    user = auth.authenticate(uid=request.GET.get('token'))
+    if user:
+        auth.login(request, user)
+    return redirect('/')
