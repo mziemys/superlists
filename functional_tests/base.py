0b5248ed86e74a8f7a16aa2b8c3ea6277e0b7364
Martynas <mziemys@gmail.com>
2020-02-29 23:46:55 +0200

    new_list view tries to assign owner but cant

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -2,6 +2,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from .server_tools import reset_database
+from selenium.webdriver.common.keys import Keys
 import time
 import os
 
@@ -33,6 +34,14 @@ class FunctionalTest(StaticLiveServerTestCase):
                         raise e
                     time.sleep(0.5)
         return modified_fn
+
+
+    def add_list_item(self, item_text):
+        num_rows = len(self.browser.find_elements_by_css_selector('#id_list_table tr'))
+        self.get_item_input_box().send_keys(item_text)
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        item_number = num_rows + 1
+        self.wait_for_row_in_list_table(f'{item_number}: {item_text}')
 
 
     @wait
